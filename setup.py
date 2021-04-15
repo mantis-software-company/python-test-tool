d54759e4984f919dc4e0f15f48e2d783fd7dff2b
Fatih Arslan <fatiharslan@mantis.com.tr>
2021-04-15 11:26:07 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 setup(
     name='mantis-python-test-tool',
-    packages=find_packages(include=['CreateString']),
+    packages=find_packages(include=['mantis-python-test-tool']),
     version='1.0.0',
     author='Derya Pelin Deniz',
     author_email="deryapelindenizzz@gmail.com",
     description="test utility",
     license="Apache Software License 2.0",
-    keywords="mantis python-test"
+    keywords="mantis python-test string-utils"
 )
