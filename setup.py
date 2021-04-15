de104b35d21d4d358671cb58b3313a2cecefe92a
pdeniz <71728555+pdeniz@users.noreply.github.com>
2021-04-15 12:22:18 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='CreateString',
+    packages=find_packages(include=['CreateString']),
+    version='0.1.0',
+    description='Create String Library',
+    author='Derya Pelin Deniz',
+    license='Mantis'
+)
