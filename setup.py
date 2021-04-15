bd3772dbe2e59ce6edf49a3a6c59790cf1127b5e
pdeniz <71728555+pdeniz@users.noreply.github.com>
2021-04-15 12:42:01 +0300

    package name

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import find_packages, setup
 setup(
-    name='CreateString',
+    name='mantis-python-test-tool',
     packages=find_packages(include=['CreateString']),
-    version='0.1.0',
-    description='Create String Library',
+    version='1.0.0',
     author='Derya Pelin Deniz',
-    license='Mantis'
+    author_email="deryapelindenizzz@gmail.com",
+    description="test utility",
+    license="Apache Software License 2.0",
+    keywords="mantis python-test"
 )
