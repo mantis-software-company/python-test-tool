58ccd818dfbb4804b9716d5385741782d6941fa8
Fatih Arslan <fatiharslan@mantis.com.tr>
2021-04-15 11:29:53 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='mantis-python-test-tool',
     packages=find_packages(include=['mantis-python-test-tool']),
-    version='1.0.0',
+    version='0.0.0',
     author='Derya Pelin Deniz',
     author_email="deryapelindenizzz@gmail.com",
     description="test utility",
