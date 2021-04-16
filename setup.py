d76a17751d745a28f47d26d5eb4e2aab9b6b2391
Derya Pelin <deryadeniz@mantis.com.tr>
2021-04-16 07:24:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='mantisTest',
-    packages='mantis_python_test_tool',
+    packages=['mantis_python_test_tool'],
     version='1.0.0',
     author='Derya Pelin Deniz',
     author_email="deryapelindenizzz@gmail.com",
