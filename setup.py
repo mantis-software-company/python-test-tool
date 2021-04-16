655c37efc201a39cae3f4ff86ea64cbb2194565f
pdeniz <71728555+pdeniz@users.noreply.github.com>
2021-04-16 11:34:06 +0300

    version number

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
-setup(
-    name='mantis_python_test_tool',
-    version='1.0.0',
-    description='test utility',
-    author='Derya Pelin Deniz',
-    author_email='deryapelindenizzz@gmail.com',
-    packages=['mantis_python_test_tool'],
-    keywords='mantis-python-test-tool'
-)
+
+file = open("VERSION", "r")
+
+setup(name='mantis_python_test_tool',
+      version='1.0.0',
+      description='test utility',
+      author='Derya Pelin Deniz',
+      author_email='deryapelindenizzz@gmail.com',
+      packages=['mantis_python_test_tool'],
+      keywords='mantis-python-test-tool')
