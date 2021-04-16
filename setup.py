ffaab2ad90d61231038fdbed821e3d1207276811
Derya Pelin <deryadeniz@mantis.com.tr>
2021-04-16 07:35:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 setup(
     name='mantis_python_test_tool',
-    packages=['mantis_python_test_tool'],
     version='1.0.0',
     author='Derya Pelin Deniz',
     author_email='deryapelindenizzz@gmail.com',
     description='test utility',
+    packages=['mantis_python_test_tool'],
     license='Apache Software License 2.0',
-    setup_requires=['nexus_uploader'],
+    install_requires=['nexus_uploader'],
     keywords='mantis'
 )
