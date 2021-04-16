662e496ae169068661587f93320e0dc936bddb6f
pdeniz <71728555+pdeniz@users.noreply.github.com>
2021-04-16 07:56:56 +0300

    mantisTest is used for name in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 setup(
-    name='mantis-python-test-tool',
-    packages=['mantis-python-test-tool'],
-    version='0.0.0',
+    name='mantisTest',
+    packages=find_packages(include=['CreateString']),
+    version='1.0.0',
     author='Derya Pelin Deniz',
-    author_email='deryapelindenizzz@gmail.com',
-    description='test utility',
-    license='Apache Software License 2.0',
-    setup_requires=['nexus_uploader'],
-    keywords='mantis'
+    author_email="deryapelindenizzz@gmail.com",
+    description="test utility",
+    license="Apache Software License 2.0",
+    keywords="mantis python-test"
 )
